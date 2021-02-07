c9dd38cde8d6781c63c84e12c93f3e8d90dca372
mohak <mohaknigam20@gmail.com>
2021-02-08 02:25:09 +0530

    get message

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -32,6 +32,13 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
+  void getMessages() async {
+    final messages = await _firestore.collection('messages').get();
+    for (var message in messages.docs) {
+      print(message.data());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
